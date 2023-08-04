cd7f909ab2b97301c923affcccb2906a6142f4c9
Ibrahim E. Aliyev <ibrahimeal@code.local>
2023-08-04 16:44:45 +0400

    added

---
+++ ConsoleApp1/Custom.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Custom
+    {
+        public static int CustomLastIndexOf(this string sentence, char letter)
+        {
+            for (int i = sentence.Length - 1; i >= 0; i--)
+            {
+                if (sentence[i] == letter)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int CustomVowelCounter(this string text)
+        {
+
+            int vowelCount = 0;
+            //char[] vowels = { 'a', 'e', 'o', 'u', 'i', 'A', 'E', 'O', 'U', 'I' };
+            string vowels = "aeouiAEOUI";
+
+            foreach (char letter in text)
+            {
+                if (!vowels.Contains(letter))
+                {
+                    vowelCount++;
+                }
+            }
+            return vowelCount;
+        }
+
+       public static string CustomTrim(this string text)
+        {
+            int start = 0;
+            int end = text.Length - 1;
+            string result = "";
+
+            while(start <= end && Char.IsWhiteSpace(text[start]))
+            {
+                start++;
+            }
+
+            while (start >= end && Char.IsWhiteSpace(text[end]))
+            {
+                end--;
+            }
+
+            for (int i = start; i < end; i++)
+            {
+                result += text[i];
+            }
+
+            return result;
+        }
+
+       public static string CustomOldTrim(this string text)
+        {
+            string result = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!Char.IsWhiteSpace(text[i]))
+                {
+                    result += text[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //string sentence = "salam salam salam";
+            //char letter = 'a';
+            //int result = sentence.CustomLastIndexOf(letter);
+            //Console.WriteLine(result);
+            #endregion
+
+
+            #region Task2
+            //string text = "Azerbaycan";
+            //int result = text.CustomVowelCounter();
+            //Console.WriteLine(result);
+            #endregion
+
+            static void AreaOfTriangle(double a, double b, double c)
+            {
+                double P = (a + b + c) / 2;
+                double area = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
+                Console.WriteLine(area);
+            }
+
+
+            #region Task3
+            //AreaOfTriangle(6, 8, 10);
+            #endregion
+
+            #region Task4
+            string text = "  Hello World ";
+            Console.WriteLine(text.CustomTrim());
+            //Console.WriteLine(text.CustomOldTrim());
+
+            #endregion
+        }
+    }
+}
